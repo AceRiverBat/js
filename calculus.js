6b65d7bd96c27b71bac2843eb429520e90494d4f
Sarah <stahar06@gmail.com>
2022-01-04 15:01:59 +0100

    Update calculus.js

--- calculus.js
+++ calculus.js
@@ -1,64 +1,33 @@
-/*
-function calculus() {
-    let x = prompt("Choisir un premier chiffre");
-    let y = prompt("Choisir un deuxième chiffre");
-    let operation = prompt("Choisir une opération");
-    
-    if(operation=="+"){
-      z=Number(x)+Number(y);
-    }
-    else if(operation=="-"){
-      z=Number(x)-Number(y);
-    }
-    else if(operation=="*"){
-      z=Number(x)*Number(y);
-    }
-    else if(operation=="/"){
-       z=Number(x)/Number(y);
-    }
-    
-    return z;
-    }
-
-calculus();
-console.log(z);
-*/
-function calculus() {
-let x = prompt("Choisir un premier chiffre");
-let y = prompt("Choisir un deuxième chiffre");
-let ope = prompt("Choisir l'opération a effectuer")
+let x = Number(prompt("Choisir un premier chiffre"));
+let y = Number(prompt("Choisir un deuxième chiffre"));
+let ope = prompt("Choisir l'opération a effectuer");
 
 function addition(){
-  if(Number(x)+Number(y));
-  console.log(Number(x)+Number(y));
+  if(x + y);
+  console.log(x + y);
 }
 function soustraction(){
-  if(Number(x)-Number(y));
-  console.log(Number(x)-Number(y));
+  if(x - y);
+  console.log(x - y);
 }
 function multiplication(){
-  if(Number(x)*Number(y));
-  console.log(Number(x)*Number(y));
+  if(x * y);
+  console.log(x * y);
 }
 function division(){
-  if(Number(x)/Number(y));
-  console.log(Number(x)/Number(y));
+  if(x / y);
+  console.log(x / y);
 }
 
-if(ope=="+"){
-  z=addition();
+if(ope == "+"){
+  addition();
 }
-else if(operation=="-"){
-  z=soustraction();
+else if(ope == "-"){
+  soustraction();
 }
-else if(operation=="*"){
-  z=multiplication();
+else if(ope == "*"){
+  multiplication();
 }
-else if(operation=="/"){
-   z=division();
+else if(ope == "/"){
+  division();
 }
-
-return z;
-}
-calculus();
-console.log(z);
