335821e44c1f74e1458231b1ae8fa38855f5a855
Sarah <stahar06@gmail.com>
2022-01-04 12:22:05 +0100

    Update calculus.js

--- calculus.js
+++ calculus.js
@@ -1,3 +1,4 @@
+/*
 function calculus() {
     let x = prompt("Choisir un premier chiffre");
     let y = prompt("Choisir un deuxième chiffre");
@@ -19,5 +20,45 @@ function calculus() {
     return z;
     }
 
+calculus();
+console.log(z);
+*/
+function calculus() {
+let x = prompt("Choisir un premier chiffre");
+let y = prompt("Choisir un deuxième chiffre");
+let ope = prompt("Choisir l'opération a effectuer")
+
+function addition(){
+  if(Number(x)+Number(y));
+  console.log(Number(x)+Number(y));
+}
+function soustraction(){
+  if(Number(x)-Number(y));
+  console.log(Number(x)-Number(y));
+}
+function multiplication(){
+  if(Number(x)*Number(y));
+  console.log(Number(x)*Number(y));
+}
+function division(){
+  if(Number(x)/Number(y));
+  console.log(Number(x)/Number(y));
+}
+
+if(ope=="+"){
+  z=addition();
+}
+else if(operation=="-"){
+  z=soustraction();
+}
+else if(operation=="*"){
+  z=multiplication();
+}
+else if(operation=="/"){
+   z=division();
+}
+
+return z;
+}
 calculus();
 console.log(z);
